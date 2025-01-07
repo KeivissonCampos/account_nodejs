37dc3cf77551fc0f1cd955949a4231c345881580
KEIVISSON CAMPOS <keivisson.dev@gmail.com>
2025-01-07 13:01:57 -0300

    :sparkles: feat: account creation completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const chalk = require("chalk")
 //internal modules
 const fs = require("fs")
 
+clearTerminal()
 operation()
 
 function operation(){
@@ -25,7 +26,13 @@ function operation(){
     .then((answer) => {
         const action = answer['action']
         clearTerminal()
-        createAccount()
+
+        if(action === 'Criar conta'){
+            createAccount()
+        }else{
+            console.log(chalk.bgRed(`Desculpe! A opção '${action}' ainda está sendo implementada`))
+            operation()
+        }
     })
     .catch((err) => console.log(err))
 }
@@ -34,7 +41,40 @@ function operation(){
 function createAccount(){
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+    buildAccount()
 }
+
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta: ',
+        }
+    ])
+    .then((answer)=>{
+        const nameAccount = answer['accountName']
+        console.info(nameAccount)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+        if(fs.existsSync(`accounts/${nameAccount}.json`)){
+            clearTerminal()
+            console.log(chalk.bgYellow.black(`A conta com o nome ${chalk.bgRed(nameAccount)} já existe, escolha outro nome!`))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${nameAccount}.json`, '{"balance": 0}', function(err){
+            console.log(err)
+        })
+        clearTerminal()
+        console.log(chalk.green(`Parabéns, a conta ${nameAccount} foi criada!`))
+        operation()
+    })
+    .catch((err)=>{console.log(err)})
+};
 
 //clear terminal
 function clearTerminal(){
