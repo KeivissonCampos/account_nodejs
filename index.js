6032c6309696a354bb13e5ccb7eb0b28d4d1efce
KEIVISSON CAMPOS <keivisson.dev@gmail.com>
2025-01-07 09:40:22 -0300

    feat: create function clearTerminal

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function operation(){
     ])
     .then((answer) => {
         const action = answer['action']
-        console.clear()
+        clearTerminal()
         createAccount()
     })
     .catch((err) => console.log(err))
@@ -34,4 +34,9 @@ function operation(){
 function createAccount(){
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
+
+//clear terminal
+function clearTerminal(){
+    console.clear()
 }
