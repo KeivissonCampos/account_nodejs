acd7ae39f7b1da96e3f5f7d2a4e2b79dd0f8efd7
KEIVISSON CAMPOS <keivisson.dev@gmail.com>
2025-01-07 09:36:26 -0300

    :construction: feat: create account

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//Modulos externos
+//external modules
 const inquirer = require("inquirer")
 const chalk = require("chalk")
 
-//Modulos internos
+//internal modules
 const fs = require("fs")
 
 operation()
@@ -24,7 +24,14 @@ function operation(){
     ])
     .then((answer) => {
         const action = answer['action']
-        console.log(action)
+        console.clear()
+        createAccount()
     })
     .catch((err) => console.log(err))
+}
+
+//create an account
+function createAccount(){
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
 }
